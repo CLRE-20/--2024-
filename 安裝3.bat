196b85e9ffa3138a21fb51806f148861037d2362
clre20 <167730039+clre20@users.noreply.github.com>
2024-12-06 17:10:35 +0800

    Add files via upload

---
+++ 安裝3.bat
@@ -0,0 +1,17 @@
+@echo on
+echo Downloading Python installer...
+powershell -Command "Invoke-WebRequest -Uri https://www.python.org/ftp/python/3.11.4/python-3.11.4.exe -OutFile python-installer.exe"
+
+echo Installing Python...
+start /wait python-installer.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+
+timeout /t 5
+
+echo Installing required Python packages...
+pip install flask flask_login flask-sqlalchemy paho-mqtt
+
+echo Cloning the GitHub repository...
+git clone https://github.com/clre20/Earthquake-power-system-2024-Topics.git
+
+echo Installation and download complete!
+pause
